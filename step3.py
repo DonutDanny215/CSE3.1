33dede48282454e37313a32c0548a0a4038d0fea
Daniel G <daniel.gil@vtsd.com>
2024-03-01 12:28:38 -0500

    As of 03/01/2024

---
+++ step3.py
@@ -0,0 +1,13 @@
+sandwich = input("What choice of sandwich would you like: Chicken ($5.25) Beef ($6.25) Tofu ($5.75)")
+total_cost
+
+if sandwich == "Cheicken":
+    total_cost = total_cost + 5.25
+elif sandwich == "Beef":
+    total_cost = total_cost + 6.25
+
+
+
+
+
+
