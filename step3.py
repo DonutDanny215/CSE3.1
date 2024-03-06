e66c82094a1f94d632667a72e3192735f81649f6
Daniel G <daniel.gil@vtsd.com>
2024-03-06 12:36:48 -0500

    As of 03/06/2024 (Updated)

--- step3.py
+++ step3.py
@@ -1,6 +1,6 @@
 sandwich_type = ""
 sandwich_price = ""
-
+subtotal = 0.0
 # Explaining Menu options
 print("What type of Sandwich would you like?")
 print("Chicken $5.25, Beef $6.25, Tofu $5.75")
@@ -16,6 +16,46 @@ elif sandwich_type == "Tofu":
 else:
     print("You chose Beef, which will be $6.25")
     subtotal += 6.25
+
+
+beverage_size = ""
+beverage_price = ""
+subtotal = 0.0
+
+print("What size of beverage would you like?")
+print("Small $1.00, Medium $1.75, Large $2.25")
+beverage_size = ("Beverage Choice: ")
+
+if beverage_size == "Small":
+    print("You chose Small, which will be $5.25")
+    subtotal += 5.25
+elif beverage_size == "Medium":
+    print("You chose Tofu, which will be $5.75")
+    subtotal += 5.75
+else:
+    print("You chose Large, which will be $6.25")
+    subtotal += 6.25
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
