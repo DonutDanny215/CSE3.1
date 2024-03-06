3938bfda463052e92b121acf079cd9dbcf19473f
Daniel G <daniel.gil@vtsd.com>
2024-03-06 12:23:20 -0500

    As of 03/06/2024

--- step3.py
+++ step3.py
@@ -1,12 +1,21 @@
-sandwich = input("What choice of sandwich would you like: Chicken ($5.25) Beef ($6.25) Tofu ($5.75)")
-total_cost
+sandwich_type = ""
+sandwich_price = ""
 
-if sandwich == "Cheicken":
-    total_cost = total_cost + 5.25
-elif sandwich == "Beef":
-    total_cost = total_cost + 6.25
+# Explaining Menu options
+print("What type of Sandwich would you like?")
+print("Chicken $5.25, Beef $6.25, Tofu $5.75")
+sandwich_type = ("Sandwich Choice: ")
 
-
+# Calculating Sandwich Price
+if sandwich_type == "Chicken":
+    print("You chose Chicken, which will be $5.25")
+    subtotal += 5.25
+elif sandwich_type == "Tofu":
+    print("You chose Tofu, which will be $5.75")
+    subtotal += 5.75
+else:
+    print("You chose Beef, which will be $6.25")
+    subtotal += 6.25
 
 
 
